79286deafd9316520e6b6e7a7f6d529cb432b48f
Gabriel Froes <gabriel@rwstudio.com.br>
2020-03-10 17:47:29 -0300

    Improve code for plugin

--- src/my_youtube_recomendation.php
+++ src/my_youtube_recomendation.php
@@ -13,11 +13,20 @@ if (!defined('ABSPATH')){
 	exit;
 }
 
+// Pasta do Plugin
+if ( ! defined( 'MY_YOUTUBE_RECOMENDATION_PLUGIN_DIR' ) ) {
+	define( 'MY_YOUTUBE_RECOMENDATION_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+
 if (!function_exists('my_youtube_recomendation_init')){
+
+    require_once MY_YOUTUBE_RECOMENDATION_PLUGIN_DIR . 'functions.php';
 
     function my_youtube_recomendation_init($content){
         if (is_single()) {  
-            $content .= '<p><strong>LISTA COM OS VÍDEOS DO YOUTUBE</strong></p>';
+            $list_videos = my_youtube_recomendation_fetch_videos();
+
+            $content .=  "<p><a href='{$list_videos[0]['link']}' target='_blank'><img src='{$list_videos[0]['thumbnail']}'><br>{$list_videos[0]['title']}</a></p>";
         }
         return $content;
     }
@@ -25,3 +34,19 @@ if (!function_exists('my_youtube_recomendation_init')){
 } // !function_exists
 
 add_filter( 'the_content', 'my_youtube_recomendation_init' );
+
+
+
+
+
+
+
+// TODO: Chamada ajax - https://stackoverflow.com/questions/17855846/using-ajax-in-a-wordpress-plugin
+add_action( 'wp_ajax_my_action', 'my_action' );
+function my_action() {
+	global $wpdb;
+	$whatever = intval( $_POST['whatever'] );
+	$whatever += 10;
+        echo $whatever;
+	wp_die();
+}
