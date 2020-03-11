0888a7b36aced044d955c3503b7d517eb648f6d8
Gabriel Froes <gabriel@rwstudio.com.br>
2020-03-11 16:00:17 -0300

    refactor + constants

--- src/my_youtube_recomendation.php
+++ src/my_youtube_recomendation.php
@@ -1,8 +1,8 @@
 <?php
 /**
 * Plugin Name: My Youtube Recomendation
-* Description: Your very first plugin!
-* Version: 1.0
+* Description: Display the last videos from a Youtube channel using Youtube Feed and keep always updated even for cached posts.
+* Version: 1.0.0
 * Author: Gabriel Froes
 * Author URI: https://twitter.com/gabrielfroes
 * License: GPLv2 or later
@@ -13,40 +13,46 @@ if (!defined('ABSPATH')){
 	exit;
 }
 
-// Pasta do Plugin
+// Plugin Slug
+if ( ! defined( 'MY_YOUTUBE_RECOMENDATION_PLUGIN_SLUG' ) ) {
+	define( 'MY_YOUTUBE_RECOMENDATION_PLUGIN_SLUG', 'my_youtube_recomendation' );
+}
+
+// Plugin Folder
 if ( ! defined( 'MY_YOUTUBE_RECOMENDATION_PLUGIN_DIR' ) ) {
 	define( 'MY_YOUTUBE_RECOMENDATION_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 }
 
 if (!function_exists('my_youtube_recomendation_init')){
 
-    require_once MY_YOUTUBE_RECOMENDATION_PLUGIN_DIR . 'functions.php';
+   require_once MY_YOUTUBE_RECOMENDATION_PLUGIN_DIR . 'functions.php';
 
     function my_youtube_recomendation_init($content){
         if (is_single()) {  
             $list_videos = my_youtube_recomendation_fetch_videos();
-
-            $content .=  "<p><a href='{$list_videos[0]['link']}' target='_blank'><img src='{$list_videos[0]['thumbnail']}'><br>{$list_videos[0]['title']}</a></p>";
+            $content .=  my_youtube_recomendation_build_list($list_videos);
         }
         return $content;
     }
 
 } // !function_exists
 
+
+// Filters
 add_filter( 'the_content', 'my_youtube_recomendation_init' );
 
+// Actions
 
 
 
 
 
-
-// TODO: Chamada ajax - https://stackoverflow.com/questions/17855846/using-ajax-in-a-wordpress-plugin
-add_action( 'wp_ajax_my_action', 'my_action' );
-function my_action() {
-	global $wpdb;
-	$whatever = intval( $_POST['whatever'] );
-	$whatever += 10;
-        echo $whatever;
-	wp_die();
-}
+// // TODO: Chamada ajax - https://stackoverflow.com/questions/17855846/using-ajax-in-a-wordpress-plugin
+// add_action( 'wp_ajax_my_action', 'my_action' );
+// function my_action() {
+// 	global $wpdb;
+// 	$whatever = intval( $_POST['whatever'] );
+// 	$whatever += 10;
+//         echo $whatever;
+// 	wp_die();
+// }
