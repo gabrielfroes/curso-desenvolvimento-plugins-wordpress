fd535fcafa788ccc7b838bf69d907751786a81f1
Gabriel Froes <gabriel@rwstudio.com.br>
2020-03-17 18:50:36 -0300

    fix bug

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,80 +1,85 @@
-let my_youtube_rec_container_id = "";
+class MyYoutubeRecommendation {
 
-function my_yt_rec_init(container_id) {
-    console.log(`%cMy Youtube Recommendation: Starting at '${container_id}'`, "background:green;color:white");
+    static data = {};
+    static containerId = "";
 
-    my_youtube_rec_container_id = container_id;
+    static async loadVideos() {
+        console.log(`%cMy Youtube Recommendation: Loading data from AJAX'`, "background:green;color:white");
 
-    let url = 'http://localhost:8080/wp-content/uploads/my-youtube-recommendation/videos.json';
-    fetch(url)
-        .then(res => res.json())
-        .then((out) => {
-            buildList(out);
-        })
-        .catch(err => {
-            console.error("%cMy Youtube Recommendation Error: JSON File was not load.", "background:pink;color:red");
+        const postData = {
+            action: 'my_youtube_recommendation_videos'
+        };
+        jQuery.post(my_yt_rec_ajax.url, postData, function (response) {
+            MyYoutubeRecommendation.data = JSON.parse(response);
+            if (MyYoutubeRecommendation.containerId != "") MyYoutubeRecommendation.buildList();
         });
-}
 
-function buildList(list) {
-
-    let theListTitle = document.createElement('h3');
-    let theList = document.createElement('div');
-
-    // Channel Info
-    let channelName = list['channel'].name;
-    let channelLink = list['channel'].link;
-    let channelAvatar = document.createElement('img');
-    channelAvatar.src = list['channel'].avatar;
-    channelAvatar.className = 'my-yt-rec-avatar';
-
-    let channelElements = Array();
-    channelElements['name'] = channelName;
-    channelElements['link'] = channelLink;
-    channelElements['avatar'] = channelAvatar;
-
-    // Title
-    theListTitle.textContent = 'Meus Vídeos';
-
-    // Full List
-    theList.className = 'my-yt-rec'
-    //theList.appendChild(theListTitle);
-
-    for (let i = 0; i < list['videos'].length; i++) {
-        theList.appendChild(buildListItem(list['videos'][i], channelElements));
     }
-    document.querySelector(`#${my_youtube_rec_container_id}`).appendChild(theList.cloneNode(true));
-}
 
-function buildListItem(item, channel) {
-    let theItem = document.createElement('div');
-    let theTitle = document.createElement('p');
-    let theThumb = document.createElement('img');
-    let theThumbLink = document.createElement('a');
-    let theTitleLink;
+    static buildList() {
+        let list = MyYoutubeRecommendation.data;
 
-    // Links
-    theThumbLink.href = item.link;
-    theThumbLink.target = '_blank';
-    theThumbLink.title = item.title;
-    theTitleLink = theThumbLink.cloneNode();
+        let theListTitle = document.createElement('h3');
+        let theList = document.createElement('div');
 
-    // Title
-    theTitle.className = 'my-yt-rec-title';
-    theTitle.textContent = item.title;
+        // Channel Info
+        let channelName = list.channel.name;
+        let channelLink = list.channel.link;
+        let channelAvatar = document.createElement('img');
+        channelAvatar.src = list.channel.avatar;
+        channelAvatar.className = 'my-yt-rec-avatar';
 
-    // Thumb
-    theThumb.className = 'my-yt-rec-thumbnail';
-    theThumb.src = item.thumbnail;
+        let channelElements = Array();
+        channelElements.name = channelName;
+        channelElements.link = channelLink;
+        channelElements.avatar = channelAvatar;
 
-    theThumbLink.appendChild(theThumb);
-    theTitleLink.appendChild(theTitle)
+        // Title
+        theListTitle.textContent = 'Meus Vídeos';
 
-    // Item
-    theItem.className = "my-yt-rec-item";
-    theItem.appendChild(theThumbLink);
-    theItem.appendChild(channel['avatar'].cloneNode(true));
-    theItem.appendChild(theTitleLink);
+        // Full List
+        theList.className = 'my-yt-rec'
+        //theList.appendChild(theListTitle);
 
-    return theItem;
-}
+        for (let i = 0; i < list.videos.length; i++) {
+            theList.appendChild(MyYoutubeRecommendation.buildListItem(list.videos[i], channelElements));
+        }
+
+        let container = document.querySelector(`#${MyYoutubeRecommendation.containerId}`);
+        container.innerHTML = '';
+        container.appendChild(theList.cloneNode(true));
+    }
+
+    static buildListItem(item, channel) {
+        let theItem = document.createElement('div');
+        let theTitle = document.createElement('p');
+        let theThumb = document.createElement('img');
+        let theThumbLink = document.createElement('a');
+        let theTitleLink;
+
+        // Links
+        theThumbLink.href = item.link;
+        theThumbLink.target = '_blank';
+        theThumbLink.title = item.title;
+        theTitleLink = theThumbLink.cloneNode();
+
+        // Title
+        theTitle.className = 'my-yt-rec-title';
+        theTitle.textContent = item.title;
+
+        // Thumb
+        theThumb.className = 'my-yt-rec-thumbnail';
+        theThumb.src = item.thumbnail;
+
+        theThumbLink.appendChild(theThumb);
+        theTitleLink.appendChild(theTitle)
+
+        // Item
+        theItem.className = "my-yt-rec-item";
+        theItem.appendChild(theThumbLink);
+        theItem.appendChild(channel['avatar'].cloneNode(true));
+        theItem.appendChild(theTitleLink);
+
+        return theItem;
+    }
+};
