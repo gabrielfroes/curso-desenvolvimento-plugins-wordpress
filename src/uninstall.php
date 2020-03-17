1249bb78ae6ebd95a020b9f004269fb1fe885622
Gabriel Froes <gabriel@rwstudio.com.br>
2020-03-17 17:12:05 -0300

    new actions

--- src/uninstall.php
+++ src/uninstall.php
@@ -4,20 +4,12 @@ if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-global $wpdb;
-
-if ( is_multisite() ) {
-	global $wpdb;
-	$blogs = $wpdb->get_results( "SELECT blog_id FROM {$wpdb->blogs}", ARRAY_A );
-	if ( $blogs ) {
-		foreach ( (array) $blogs as $blog ) {
-			switch_to_blog( $blog['blog_id'] );
-			$wpdb->query( "DELETE FROM $wpdb->postmeta WHERE meta_key LIKE '_rpbt_related_posts%'" );
-			delete_transient( 'rpbt_related_posts_flush_cache' );
-		}
-		restore_current_blog();
-	}
-} else {
-	$wpdb->query( "DELETE FROM $wpdb->postmeta WHERE meta_key LIKE '_rpbt_related_posts%'" );
-	delete_transient( 'rpbt_related_posts_flush_cache' );
+function my_youtube_recommendation_uninstall() {
+	delete_option('my_yt_rec');
+	
+	// TODO: Refazer e testar a remoção da pasta e também do arquivo JSON que o plugin gera na pasta uploads do Wordpress.
+	$json_file_path = $upload_dir['basedir'].'/'.MY_YOUTUBE_RECOMMENDATION_PLUGIN_SLUG.'/'.MY_YOUTUBE_RECOMMENDATION_JSON_FILENAME;
+	wp_delete_file($json_file_path);
 }
+
+register_deactivation_hook( __FILE__, 'my_youtube_recommendation_uninstall' );
