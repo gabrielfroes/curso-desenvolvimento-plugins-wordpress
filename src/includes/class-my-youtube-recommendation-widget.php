b0d81096a0cec2f2f81f1b36730611295860db99
Gabriel Froes <gabriel@rwstudio.com.br>
2020-04-03 16:35:45 -0300

    widget is done

--- src/includes/class-my-youtube-recommendation-widget.php
+++ src/includes/class-my-youtube-recommendation-widget.php
@@ -7,21 +7,26 @@ if ( ! class_exists( 'My_Youtube_Recommendation_Widget' ) ) {
         // Main constructor
         public function __construct() {
             parent::__construct(
-                    'my_youtube_recommendation_widget',
-                    MY_YOUTUBE_RECOMMENDATION_NAME,
-                    array(
-                        'customize_selective_refresh' => true,
-                    )
-                );
+                'my_youtube_recommendation_widget',
+                __('My Youtube Recommendation'),
+                array(
+                    'customize_selective_refresh' => true,
+                )
+            );
+
+            add_action( 'widgets_init', array( $this, 'init' ) );
+        }
+
+        public function init() {
+            register_widget( 'My_Youtube_Recommendation_Widget' );
         }
 
         // The widget form (for the backend )
         public function form( $instance ) {	
             // Set widget defaults
             $defaults = array(
-                'title'         => 'Last Videos',
-                'limit'         => '3',
-                'max_height'    => '',
+                'title'         => __('Last Videos'),
+                'limit'         => '3'
             );
             
             // Parse current settings with defaults
@@ -29,23 +34,17 @@ if ( ! class_exists( 'My_Youtube_Recommendation_Widget' ) ) {
 
             <?php // Title ?>
             <p>
-                <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Title', 'text_domain' ); ?></label>
+                <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php echo __('Title'); ?></label>
                 <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
             </p>
             <?php ?>
 
             <?php // Limit ?>
-            <!-- TODO: Require Validation: Range 1 to 15 -->
             <p>
-                <label for="<?php echo esc_attr( $this->get_field_id( 'limit' ) ); ?>"><?php _e( 'List limit', 'text_domain' ); ?> (15 max)</label>
-                <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'limit' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'limit' ) ); ?>" type="number" min="1" max="15" value="<?php echo esc_attr( $limit ); ?>" />
+                <label for="<?php echo esc_attr( $this->get_field_id( 'limit' ) ); ?>"><?php echo __('Number of videos to show:'); ?></label>
+                <input class="tiny-text" id="<?php echo esc_attr( $this->get_field_id( 'limit' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'limit' ) ); ?>" type="number" step="1" min="1" max="15" value="<?php echo esc_attr( $limit ); ?>" size="3" /> (15 max)
             </p>
 
-            <?php // Container Max Height ?>
-            <p>
-                <label for="<?php echo esc_attr( $this->get_field_id( 'max_height' ) ); ?>"><?php _e( 'Max Height', 'text_domain' ); ?> (in pixels)</label>
-                <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'max_height' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'max_height' ) ); ?>" type="number" value="<?php echo esc_attr( $max_height ); ?>" />
-            </p>
             <?php
         }
 
@@ -54,7 +53,6 @@ if ( ! class_exists( 'My_Youtube_Recommendation_Widget' ) ) {
             $instance = $old_instance;
             $instance['title']      = isset( $new_instance['title'] ) ? wp_strip_all_tags( $new_instance['title'] ) : '';
             $instance['limit']      = isset( $new_instance['limit'] ) ? wp_strip_all_tags( $new_instance['limit'] ) : '';
-            $instance['max_height'] = isset( $new_instance['max_height'] ) ? wp_strip_all_tags( $new_instance['max_height'] ) : '';
             return $instance;
         }
 
@@ -67,7 +65,6 @@ if ( ! class_exists( 'My_Youtube_Recommendation_Widget' ) ) {
             // Check the widget options
             $title      = isset( $instance['title'] ) ? apply_filters( 'title', $instance['title'] ) : '';
             $limit      = isset( $instance['limit'] ) ? apply_filters( 'limit', $instance['limit'] ) : '';
-            $max_height = isset( $instance['max_height'] ) ? apply_filters( 'max_height', $instance['max_height'] ) : '';
 
             // WordPress core before_widget hook (always include )
             echo $before_widget;
@@ -75,11 +72,12 @@ if ( ! class_exists( 'My_Youtube_Recommendation_Widget' ) ) {
             ?>
             <div class="widget-text wp_widget_plugin_box">
                 <?php echo ( $title ) ? $before_title . $title . $after_title : ''; ?>
-                <div id='$widget_unique_id'></div>
+                <div id='<?php echo $widget_unique_id ?>'><div style='text-align:center'><?php echo __('Loading...') ?></div></div>
             </div>
             <script>
                 (function ($) {
                     $(function () {
+                        MyYoutubeRecommendation.limit = <?php echo $limit ?>;
                         MyYoutubeRecommendation.loadVideos();
                         MyYoutubeRecommendation.containerId = '<?php echo $widget_unique_id ?>';
                     });
@@ -94,16 +92,3 @@ if ( ! class_exists( 'My_Youtube_Recommendation_Widget' ) ) {
     }
 
 } // !class_exists
-
-// Register the widget
-if ( ! function_exists( 'my_youtube_recommendation_widget' ) ){
-
-    function my_youtube_recommendation_widget() {
-        register_widget( 'My_Youtube_Recommendation_Widget' );
-    }
-    
-} // !function_exists
-
-// TODO: Tentar usar o mesmo tipo de inicialização do página de administração utilizando o __constructor.
-
-add_action( 'widgets_init', 'my_youtube_recommendation_widget' );
