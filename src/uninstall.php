7284648ed6e4ec191813cbbf4a1fb80a0d22dfa7
Gabriel Froes <gabriel@rwstudio.com.br>
2020-04-03 16:36:10 -0300

    update uninstall process

--- src/uninstall.php
+++ src/uninstall.php
@@ -4,12 +4,20 @@ if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-function my_youtube_recommendation_uninstall() {
-	delete_option('my_yt_rec');
-	
-	// TODO: Refazer e testar a remoção da pasta e também do arquivo JSON que o plugin gera na pasta uploads do Wordpress.
-	$json_file_path = $upload_dir['basedir'].'/'.MY_YOUTUBE_RECOMMENDATION_PLUGIN_SLUG.'/'.MY_YOUTUBE_RECOMMENDATION_JSON_FILENAME;
-	wp_delete_file($json_file_path);
+if ( !function_exists( 'WP_Filesystem' ) ) { 
+    require_once ABSPATH . '/wp-admin/includes/file.php'; 
+} 
+
+if ( ! function_exists( 'my_youtube_recommendation_uninstall' ) ) {
+
+	function my_youtube_recommendation_uninstall() {
+		delete_option('my_yt_rec');
+
+		$json_folder = $upload_dir['basedir'] . '/my-youtube-recommendation';
+		$wp_filesystem = WP_Filesystem_Direct();
+		$wp_filesystem->delete($json_folder, true);
+
+	}
 }
 
 register_deactivation_hook( __FILE__, 'my_youtube_recommendation_uninstall' );
