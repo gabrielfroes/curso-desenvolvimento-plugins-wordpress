78e473f4c6cdedbff9047c20c9f75b6151c3bbd5
Gabriel Froes <gabriel@rwstudio.com.br>
2020-04-08 15:33:37 -0300

    Uninstall now works

--- src/uninstall.php
+++ src/uninstall.php
@@ -4,19 +4,18 @@ if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-if ( !function_exists( 'WP_Filesystem' ) ) { 
-    require_once ABSPATH . '/wp-admin/includes/file.php'; 
-} 
-
 if ( ! function_exists( 'my_youtube_recommendation_uninstall' ) ) {
 
-	function my_youtube_recommendation_uninstall() {
-		delete_option('my_yt_rec');
-
-		$json_folder = $upload_dir['basedir'] . '/my-youtube-recommendation';
-		$wp_filesystem = WP_Filesystem_Direct();
-		$wp_filesystem->delete($json_folder, true);
+	function my_youtube_recommendation_uninstall2() {
+        delete_option('my_yt_rec');
+        
+        $upload_dir     = wp_upload_dir();
+		$json_folder 	= $upload_dir['basedir'] . '/my-youtube-recommendation' ;
+		$json_file 		= $json_folder . '/my-yt-rec.json';
+		unlink($json_file);
+        rmdir($json_folder);
 	}
+	
 }
 
 register_uninstall_hook( __FILE__, 'my_youtube_recommendation_uninstall' );
