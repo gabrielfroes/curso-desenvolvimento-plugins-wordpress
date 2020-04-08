9804808df630329c509efba73147e71f024fec20
Gabriel Froes <gabriel@rwstudio.com.br>
2020-04-08 15:35:26 -0300

    Fixbug on listsCallbacks

--- src/public/js/loader.js
+++ src/public/js/loader.js
@@ -1,7 +1,7 @@
 (function ($) {
     $(function () {
         MyYoutubeRecommendation.loadVideos(my_yt_rec_ajax.url).then((value) => {
-            MyYoutubeRecommendation.lists.forEach((item) => {
+            MyYoutubeRecommendation.listsCallBacks.forEach((item) => {
                 item.callback(value, item.container, item.layout, item.limit);
             })
         });
